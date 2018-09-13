9834da812f4865a2726f1d4d77c0d50e44f94e49
Zaven17 <43119795+Zaven17@users.noreply.github.com>
2018-09-13 22:28:07 +0400

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,11 @@
+alert("Homework");
+const a = 15;
+  const b = 'Zaven';
+  const c = [12, 13, 21,28];
+  if(a+c[1]===c[2]){
+    console.log(b)
+  }else if(c[0]+b==='12Zaven'){
+    console.log('yes');
+  }else{
+  console.log("no");
+  }
